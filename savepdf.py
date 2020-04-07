2dc9f83de35fadfaaeb816e95269576910863c47
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-04-07 15:37:45 +0700

    Update savepdf.py

--- savepdf.py
+++ savepdf.py
@@ -4,5 +4,5 @@ from tqdm import tqdm
 with open("listpdf.txt","r",encoding="utf-8-sig") as f:
     listpdf_temp = [i.strip() for i in f.readlines()]
 print("Save to web.archive.org")
-for i in tqdm.tqdm(listpdf_temp):
+for i in tqdm(listpdf_temp):
 	print(archivenow.push("https://ddc.moph.go.th/viralpneumonia/file/situation/"+i,"ia"))
