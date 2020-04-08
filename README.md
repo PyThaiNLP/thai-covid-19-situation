932cc9d90331e135f3252e6b2829dc4f4b8570ed
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-04-08 14:12:26 +0700

    Update code

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Thai covid-19 situation
 
 ![MOPH Archive](https://github.com/PyThaiNLP/thai-covid-19-situation/workflows/MOPH%20Archive/badge.svg)
+![Save to Archive](https://github.com/PyThaiNLP/thai-covid-19-situation/workflows/Save%20to%20Archive/badge.svg)
 
 Thai covid-19 situation text file from MOPH.
 
--- savepdf.py
+++ savepdf.py
@@ -1,8 +1,10 @@
 from archivenow import archivenow
+import time
 from tqdm import tqdm
 
 with open("listpdf.txt","r",encoding="utf-8-sig") as f:
     listpdf_temp = [i.strip() for i in f.readlines()]
 print("Save to web.archive.org")
 for i in tqdm(listpdf_temp):
-	print(archivenow.push("https://ddc.moph.go.th/viralpneumonia/file/situation/"+i,"ia"))
+    print(archivenow.push("https://ddc.moph.go.th/viralpneumonia/file/situation/"+i,"ia"))
+    time.sleep(1)
