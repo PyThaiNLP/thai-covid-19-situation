72af06b99e870bd9c1addcd402fc2c05b07191c7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2021-01-05 17:47:30 +0700

    Update build.py

--- build.py
+++ build.py
@@ -5,6 +5,40 @@ import wget
 import requests
 import tqdm
 from archivenow import archivenow
+from bs4 import BeautifulSoup
+import re
+
+with open("listpdf.txt","r") as f:
+  list_file = [i.strip() for i in f.readlines()]
+
+oldpage = requests.get("https://ddc.moph.go.th/viralpneumonia/situation_more.php", timeout=30)
+page = requests.get("https://ddc.moph.go.th/viralpneumonia/situation.php", timeout=30)
+soup= BeautifulSoup(oldpage.text, "html.parser")
+links = soup.find_all('a', href=re.compile(r'(\.pdf)'))
+for i in links:
+  check = 'file/situation/' in i['href']
+  temp = i['href'].replace('file/situation/','')
+  if temp not in list_file and check:
+    list_file.append(temp)
+
+soup= BeautifulSoup(page.text, "html.parser")
+links = soup.find_all('a', href=re.compile(r'(\.pdf)'))
+for i in links:
+  check = 'file/situation/' in i['href']
+  temp = i['href'].replace('file/situation/','')
+  if temp not in list_file and check:
+    list_file.append(temp)
+
+txt = ""
+i= 0
+while i < len(list_file):
+  txt += list_file[i]
+  if i != len(list_file) -1:
+    txt += '\n'
+  i+=1
+
+with open("listpdf.txt","w") as f:
+  f.write(txt)
 
 with open("listpdf.txt","r",encoding="utf-8-sig") as f:
     listpdf_temp = [i.strip() for i in f.readlines()]
